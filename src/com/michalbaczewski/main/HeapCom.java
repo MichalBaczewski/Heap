4dc977a7ea856bf49479fc3a897763a824c7696e
MichalBaczewski <michal.baczewski@outlook.com>
2018-07-08 11:23:02 +0200

    Dodano metodę remove().

--- src/com/michalbaczewski/main/HeapCom.java
+++ src/com/michalbaczewski/main/HeapCom.java
@@ -6,26 +6,55 @@ public class HeapCom {
 
     private int[] heap;
     private int lastIndex;
+    private int index;
 
     public HeapCom(int size) {
         this.heap = new int[size];
     }
 
     void add(int value) {
-        int index = lastIndex;
+        int currentIndex = lastIndex;
         heap[lastIndex++] = value;
         int tmp = 0;
-        while (index > 0) {
-            if (heap[index] > getParent(index)) {
-                tmp = getParent(index);
-                heap[getParentIndex(index)] = heap[index];
-                heap[index] = tmp;
+        while (currentIndex > 0) {
+            if (heap[currentIndex] > getParent(currentIndex)) {
+                tmp = getParent(currentIndex);
+                heap[getParentIndex(currentIndex)] = heap[currentIndex];
+                heap[currentIndex] = tmp;
             }
-            index = getParentIndex(index);
+            currentIndex = getParentIndex(currentIndex);
         }
     }
 
-    int getParentIndex(int index) {
+    public int remove() {
+        if (!hasNodes()) {
+            throw new IndexOutOfBoundsException();
+        }
+        int elementToRemove = heap[0];
+        int currentIndex = 0;
+        int greaterChildIndex = getGreaterChildIndex(0);
+        int lastValue = heap[--lastIndex];
+        while (greaterChildIndex < lastIndex && heap[greaterChildIndex] > lastValue) {
+            heap[currentIndex] = heap[greaterChildIndex];
+            currentIndex = greaterChildIndex;
+            greaterChildIndex = getGreaterChildIndex(currentIndex);
+        }
+        heap[currentIndex] = lastValue;
+        return elementToRemove;
+    }
+
+    private int getGreaterChildIndex(int index) {
+        if (hasRightChild(index)) {
+            return getLeftChildIndex(index) > getRightChild(index)
+                    ? getLeftChildIndex(index) : getRightChildIndex(index);
+        } else if (hasLeftChild(index)) {
+            return getLeftChildIndex(index);
+        } else {
+            return lastIndex + 1;
+        }
+    }
+
+    private int getParentIndex(int index) {
         if (index != 0) {
             return (index - 1) / 2;
         } else {
@@ -37,8 +66,40 @@ public class HeapCom {
         return heap[getParentIndex(index)];
     }
 
+    int getLeftChildIndex(int index) {
+        if (hasLeftChild(index)) {
+            return 2 * index + 1;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    int getRightChildIndex(int index) {
+        if (hasRightChild(index)) {
+            return 2 * index + 2;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private int getRightChild(int index) {
+        return heap[getRightChildIndex(index)];
+    }
+
+    public boolean hasLeftChild(int index) {
+        return (2 * index + 1) < lastIndex;
+    }
+
+    private boolean hasRightChild(int index) {
+        return (2 * index + 2) < lastIndex;
+    }
+
+    private boolean hasNodes() {
+        return lastIndex > 0;
+    }
+
     @Override
     public String toString() {
-        return Arrays.toString(heap);
+        return Arrays.toString(Arrays.copyOfRange(heap, 0, lastIndex));
     }
 }
--- src/com/michalbaczewski/main/HeapComTest.java
+++ src/com/michalbaczewski/main/HeapComTest.java
@@ -3,13 +3,16 @@ package com.michalbaczewski.main;
 public class HeapComTest {
 
     public static void main(String[] args) {
-        HeapCom heap = new HeapCom(5);
-        heap.add(5);
-        System.out.println(heap.toString());
-        heap.add(6);
-        heap.add(7);
-        heap.add(2);
+        HeapCom heap = new HeapCom(10);
+        heap.add(9);
+        heap.add(53);
+        heap.add(23);
+        heap.add(66);
         heap.add(3);
+        heap.add(87);
+        heap.add(24);
+        System.out.println(heap.toString());
+        System.out.println(heap.remove());
         System.out.println(heap.toString());
     }
 
